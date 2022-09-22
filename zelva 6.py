db1f798b827e789e5777436982265bfcc5cfd347
Cagis998 <matyas.caganek@educanet.cz>
2022-09-22 10:38:22 +0200

    Zelva Šesti uhelník

---
+++ zelva 6.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+
+def sestiuhlenik():
+    for i in range(6):
+        t.forward(100)
+        t.left(60)
+
+def vypln():
+    
+ t.left(60)
+ t.forward(100)
+ t.right(120)
+ t.forward(100)
+
+sestiuhlenik()
+for i in range(5):
+    vypln()
+    t.left(120)
