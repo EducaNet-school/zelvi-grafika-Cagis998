1170c1227948b558dcee830f5fbae19625ff02af
Cagis998 <matyas.caganek@educanet.cz>
2022-11-06 20:22:03 +0300

    Nasobilka

---
+++ Nasobilka.py
@@ -0,0 +1,7 @@
+cislo = int(input ("vlož číslo: "))      
+pocitej = 1        
+print ("Násobilka čísla: ", cislo)    
+while pocitej <= 10:    
+    cislo = cislo * 1    
+    print (cislo * pocitej)    
+    pocitej += 1  
